505e5d52b607f75fb4b10eeca7299ea35a0e1fbb
Christopher Kennedy <chris@onebrightlight.com>
2018-09-15 12:02:15 -0700

    Tweaked mobile view, reduced JavaScript for toggling menu
    Signed-off-by: Christopher Kennedy <chris@onebrightlight.com>

--- templates/_js/navigation.js
+++ templates/_js/navigation.js
@@ -1,42 +1,19 @@
-function hasClass(ele, cls) {
-  return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
-}
-
-function addClass(ele, cls) {
-  if (!hasClass(ele, cls)) ele.className += " " + cls;
-}
-
-function removeClass(ele, cls) {
-  if (hasClass(ele, cls)) {
-    var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-    ele.className = ele.className.replace(reg, " ");
-  }
-}
-
 function init() {
   document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
   document.getElementById("close-menu").addEventListener("click", toggleMenu);
   document.getElementById("search-menu-toggle").addEventListener("click", toggleSearch);
 }
 
-//The actual fuction
-function toggleMenu() {
+function toggleMenu(e) {
   var ele = document.getElementsByTagName("body")[0];
-  if (!hasClass(ele, "open")) {
-    addClass(ele, "open");
-  } else {
-    removeClass(ele, "open");
-  }
+  e.preventDefault();
+  ele.classList.toggle("open");
 }
 
 function toggleSearch(e) {
   var ele = document.getElementById("global-search-bar");
   e.preventDefault();
-  if (!hasClass(ele, "lg:flex")) {
-    addClass(ele, "lg:flex");
-  } else {
-    removeClass(ele, "lg:flex");
-  }
+  ele.classList.toggle("lg:flex");
 }
 
 //Prevent the function to run before the document is loaded
