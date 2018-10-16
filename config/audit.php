5570c9ab6808816b63d93843acdc8c0b77328260
Christopher Kennedy <chris@onebrightlight.com>
2018-10-16 10:14:10 -0700

    Added Audit config
    Signed-off-by: Christopher Kennedy <chris@onebrightlight.com>

---
+++ config/audit.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // How many days to keep log entries around
+    'pruneDays'          => 15,
+
+    // Enable logging
+    'enabled'            => true,
+
+    // Enable geolocation status
+    'enabledGeolocation' => false,
+];
