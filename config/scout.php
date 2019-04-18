7d097dbae9f36e9f234aeec804c4e7a65089105f
Christopher Kennedy <chris@onebrightlight.com>
2019-04-18 13:54:35 -0700

    Craft plugin and CMS config updates
    Signed-off-by: Christopher Kennedy <chris@onebrightlight.com>

--- config/scout.php
+++ config/scout.php
@@ -486,25 +486,6 @@ return [
       'transformer' => new eventsTransform(),
     ],
     // END MEETINGS INDEX
-    // BEGIN CALENDAR INDEX
-    [
-      'indexName' => getenv('ENVIRONMENT') . '_calendars',
-      'elementType' => \Solspace\Calendar\Elements\Event::class,
-      'criteria' => [
-        'calendar' => 'events',
-        'with' => ['boardsCommissions', 'departments', 'officials', 'projects', 'topics']
-      ],
-      'transformer' => new eventsTransform(),
-    ],
-    [
-      'indexName' => getenv('ENVIRONMENT') . '_calendars',
-      'elementType' => \Solspace\Calendar\Elements\Event::class,
-      'criteria' => [
-        'calendar' => 'meetings',
-      ],
-      'transformer' => new eventsTransform(),
-    ],
-    // END CALENDAR INDEX
     // BEGIN STAFF INDEX
     [
       'indexName' => getenv('ENVIRONMENT') . '_staff',
@@ -570,5 +551,454 @@ return [
       },
     ],
     // END TEAMS INDEX
+    // BEGIN GOVERNMENT INDEX
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'officials',
+        'type' => 'officials'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 1,
+          'url' => $entry->uri,
+          'name' => $entry->groupHeadName,
+          'portrait' => ! empty($entry->portrait[0]) ? (string) $entry->portrait[0]->url : null,
+          'bio' => strip_tags($entry->groupHeadBio),
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'officials',
+        'type' => 'officialsRedirect'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 1,
+          'url' => $entry->redirectUrl,
+          'portrait' => ! empty($entry->portrait[0]) ? (string) $entry->portrait[0]->url : null,
+          'name' => $entry->groupHeadName,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'departments',
+        'type' => 'departments'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 2,
+          'url' => $entry->uri,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'leadIn' => (string) $entry->leadIn,
+          'ctaButtonText' => ! empty($entry->ctaButton->text) ? (string) $entry->ctaButton->text : null,
+          'headName' => $entry->groupHeadName,
+          'headBio' => strip_tags($entry->groupHeadBio),
+          'headPortrait' => ! empty($entry->portrait[0]) ? (string) $entry->portrait[0]->url : null,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'departments',
+        'type' => 'departmentRedirect'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 2,
+          'url' => $entry->redirectUrl,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'departments',
+        'type' => 'cityCouncil'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 2,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'url' => $entry->uri,
+          'leadIn' => (string) $entry->leadIn,
+          'ctaButtonText' => ! empty($entry->ctaButton->text) ? (string) $entry->ctaButton->text : null,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'boardsCommissions',
+        'type' => 'boardsCommissions'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'sectionRank' => 3,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'url' => $entry->uri,
+          'leadIn' => (string) $entry->leadIn,
+          'ctaButtonText' => ! empty($entry->ctaButton->text) ? (string) $entry->ctaButton->text : null,
+          'about' => strip_tags($entry->about),
+          'legacyBody' => strip_tags($entry->legacyBody),
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'staff',
+        'type' => 'newStaff',
+        'with' => ['portrait', 'department']
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 4,
+          'portrait' => ! empty($entry->portrait[0]) ? (string) $entry->portrait[0]->url : null,
+          'jobTitle' => ! empty($entry->jobTitle) ? (string) $entry->jobTitle : (string) $entry->staffImportJobTitle,
+          'bio' => (string) $entry->bio,
+          'email' => ! empty($entry->emailAddress) ? (string) $entry->emailAddress : str_replace("@oaklandnet.com", "@oaklandca.gov", $entry->staffImportEmail),
+          'department' => ! empty($entry->departments[0]) ? (string) $entry->departments[0]->title : $entry->staffImportDepartment,
+          'employmentType' => (string) $entry->employmentType->label,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_government',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'volunteers',
+        'type' => 'volunteers'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 5,
+          'portrait' => ! empty($entry->portrait->one()) ? (string) $entry->portrait->one()->url : null,
+          'jobTitle' => ! empty($entry->jobTitle) ? (string) $entry->jobTitle : null,
+          'bio' => (string) $entry->bio,
+          'email' => ! empty($entry->emailAddress) ? (string) $entry->emailAddress : null,
+          'department' => ! empty($entry->departmentOfficialBoardCommission->one()) ? (string) $entry->departmentOfficialBoardCommission->one()->title : null,
+        ];
+      },
+    ],
+    // END GOVERNMENT INDEX
+    // BEGIN PUBLIC INDEX
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'services',
+        'type' => 'services',
+        'with' => ['boardsCommissions', 'departments', 'officials', 'projects', 'topics', 'transactionBody']
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials as $value)
+          $officials[] = $value->title;
+        $projects = [];
+        foreach($entry->projects as $value)
+          $projects[] = $value->title;
+        $topics = [];
+        foreach($entry->topics as $value)
+          $topics[] = $value->title;
+        $body = [];
+        foreach ($entry->transactionBody as $block) {
+          $body[] = strip_tags($block->text);
+        }
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 1,
+          'leadIn' => (string) $entry->leadIn,
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'officials' => $officials,
+          'projects' => $projects,
+          'topics' => $topics,
+          'body' => $body,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'services',
+        'type' => 'services311'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 1,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'leadIn' => (string) $entry->leadIn,
+          'body' => (string) $entry->body,
+          'urgentIssuesDescription' => (string) $entry->urgentIssuesDescription,
+          'nonUrgentIssuesDescription' => (string) $entry->nonUrgentIssuesDescription,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'resources',
+        'type' => 'resources',
+        'with' => ['boardsCommissions', 'departments', 'officials', 'projects', 'topics', 'transactionBody']
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials as $value)
+          $officials[] = $value->title;
+        $projects = [];
+        foreach($entry->projects as $value)
+          $projects[] = $value->title;
+        $topics = [];
+        foreach($entry->topics as $value)
+          $topics[] = $value->title;
+        $body = [];
+        foreach ($entry->transactionBody as $block) {
+          $body[] = strip_tags($block->text);
+        }
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 2,
+          'resourceImage' => ! empty($entry->resourceImage->one()) ? (string) $entry->resourceImage->one()->url : null,
+          'leadIn' => (string) $entry->leadIn,
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'officials' => $officials,
+          'projects' => $projects,
+          'topics' => $topics,
+          'body' => $body,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'resources',
+        'type' => 'videos',
+        'with' => ['boardsCommissions', 'departments', 'officials', 'projects', 'topics', 'transactionBody']
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials as $value)
+          $officials[] = $value->title;
+        $projects = [];
+        foreach($entry->projects as $value)
+          $projects[] = $value->title;
+        $topics = [];
+        foreach($entry->topics as $value)
+          $topics[] = $value->title;
+        $body = [];
+        foreach ($entry->transactionBody as $block) {
+          $body[] = strip_tags($block->text);
+        }
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 2,
+          'leadIn' => (string) $entry->leadIn,
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'officials' => $officials,
+          'projects' => $projects,
+          'topics' => $topics,
+          'body' => $body,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'topics'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions->all() as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments->all() as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials->all() as $value)
+          $officials[] = $value->title;
+        $projects = [];
+        foreach($entry->projects->all() as $value)
+          $projects[] = $value->title;
+        $topics = [];
+        foreach($entry->topics->all() as $value)
+          $topics[] = $value->title;
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 3,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'leadIn' => (string) $entry->leadIn,
+          'about' => strip_tags($entry->about),
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'officials' => $officials,
+          'projects' => $projects,
+          'topics' => $topics,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_public',
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'projects'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $milestones = [];
+        foreach ($entry->timeline->all() as $block) {
+          $milestones[] = [
+            'name' => (string) $block->milestoneName,
+            'dates' => (string) $block->milestoneDates
+          ];
+        }
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions->all() as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments->all() as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials->all() as $value)
+          $officials[] = $value->title;
+        $topics = [];
+        foreach($entry->topics->all() as $value)
+          $topics[] = $value->title;
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 4,
+          'banner' => ! empty($entry->banner->one()) ? (string) $entry->banner->one()->url : null,
+          'leadIn' => (string) $entry->leadIn,
+          'about' => (string) $entry->about,
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'officials' => $officials,
+          'topics' => $topics,
+        ];
+      },
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_news',
+      'indexSettings' => [
+        'settings' => [
+            'attributesForFaceting' => [
+              'boardsCommissions',
+              'departments',
+              'projects',
+              'officials',
+              'topics'
+            ],
+        ],
+        'forwardToReplicas' => 'true',
+      ],
+      'elementType' => \craft\elements\Entry::class,
+      'criteria' => [
+        'section' => 'news'
+      ],
+      'transformer' => function(craft\elements\Entry $entry) {
+        $boardsCommissions = [];
+        foreach($entry->boardsCommissions->all() as $value)
+          $boardsCommissions[] = $value->title;
+        $departments = [];
+        foreach($entry->departments->all() as $value)
+          $departments[] = $value->title;
+        $officials = [];
+        foreach($entry->officials->all() as $value)
+          $officials[] = $value->title;
+        $projects = [];
+        foreach($entry->projects->all() as $value)
+          $projects[] = $value->title;
+        $topics = [];
+        foreach($entry->topics->all() as $value)
+          $topics[] = $value->title;
+        return [
+          'title' => $entry->title,
+          'url' => $entry->uri,
+          'sectionRank' => 5,
+          'newsImageSrc' => ! empty($entry->newsImage->one()) ? $entry->newsImage->one()->indexImage->src() : null,
+          'newsImage' => ! empty($entry->newsImage->one()) ? $entry->newsImage->one()->indexImage->toArray() : null,
+          'summary' => strip_tags($entry->summary),
+          'body' => strip_tags($entry->body),
+          'mediaContact' => (string) $entry->mediaContact,
+          'boardsCommissions' => $boardsCommissions,
+          'departments' => $departments,
+          'projects' => $projects,
+          'officials' => $officials,
+          'topics' => $topics,
+        ];
+      },
+    ],
+    // END PUBLIC INDEX
+    // BEGIN CALENDAR INDEX
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_calendars',
+      'elementType' => \Solspace\Calendar\Elements\Event::class,
+      'criteria' => [
+        'calendar' => 'events',
+        'with' => ['boardsCommissions', 'departments', 'officials', 'projects', 'topics']
+      ],
+      'transformer' => new eventsTransform(),
+    ],
+    [
+      'indexName' => getenv('ENVIRONMENT') . '_calendars',
+      'elementType' => \Solspace\Calendar\Elements\Event::class,
+      'criteria' => [
+        'calendar' => 'meetings',
+      ],
+      'transformer' => new eventsTransform(),
+    ],
+    // END CALENDAR INDEX
   ],
 ];
