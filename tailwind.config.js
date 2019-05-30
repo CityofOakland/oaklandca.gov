2b4a756b36e8b7d42df38c57a4fe99848de54f82
Christopher Kennedy <chris@onebrightlight.com>
2019-05-30 16:01:27 -0700

    Fixed spacing bug in new header
    Signed-off-by: Christopher Kennedy <chris@onebrightlight.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,124 +1,93 @@
 module.exports = {
   theme: {
     colors: {
-      "transparent": "transparent",
-      "shark": "#212529",
+      transparent: "transparent",
+      shark: "#212529",
       "catalina-blue-transparent": "rgba(3, 26, 122, .33)",
-      "celeste": "#cfd0ce",
-      "cararra": "#e9eae7",
-      "white": "#ffffff",
-      "apple": "#6db943",
+      celeste: "#cfd0ce",
+      cararra: "#e9eae7",
+      white: "#ffffff",
+      apple: "#6db943",
       "alert-red": "#f31431",
       "warning-yellow": "#ffae42",
       "camouflage-green": "#757773",
-      "parsley": "#1e4a18",
+      parsley: "#1e4a18",
       "curious-blue": "#398cd3",
-      "astronaut": "#255375",
+      astronaut: "#255375",
 
-      "alabaster": "#f9f9f9",
+      alabaster: "#f9f9f9",
       "wild-sand": "#f5f5f5",
       "catalina-blue": "#031a7a",
       "fun-green": "#008943",
-      "reef": "#b7ffa4",
-      "tundora": "#404040",
+      reef: "#b7ffa4",
+      tundora: "#404040",
     },
     screens: {
-      "sm": "576px",
-      "md": "768px",
-      "lg": "992px",
-      "xl": "1200px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
     },
     fontFamily: {
-      "sans": [
+      sans: [
         "Montserrat",
         "system-ui",
         "BlinkMacSystemFont",
         "-apple-system",
-        "Segoe UI",
-        "Roboto",
-        "Oxygen",
-        "Ubuntu",
-        "Cantarell",
-        "Fira Sans",
-        "Droid Sans",
         "Helvetica Neue",
         "sans-serif",
       ],
-      "serif-heading": [
-        "Domine",
-        "Constantia",
-        "Lucida Bright",
-        "Lucidabright",
-        "Lucida Serif",
-        "Lucida",
-        "DejaVu Serif",
-        "Bitstream Vera Serif",
-        "Liberation Serif",
-        "Georgia",
-        "serif",
-      ],
-      "serif-body": [
-        "Merriweather",
-        "Constantia",
-        "Lucida Bright",
-        "Lucidabright",
-        "Lucida Serif",
-        "Lucida",
-        "DejaVu Serif",
-        "Bitstream Vera Serif",
-        "Liberation Serif",
-        "Georgia",
-        "serif",
-      ],
-      "mono": [
+      "serif-heading": ["Domine", "Constantia", "Georgia", "serif"],
+      "serif-body": ["Merriweather", "Georgia", "serif"],
+      mono: [
         "Menlo",
         "Monaco",
         "Consolas",
         "Liberation Mono",
         "Courier New",
         "monospace",
-      ]
+      ],
     },
     fontSize: {
-      "xs": ".75rem",     // 12px
-      "sm": ".875rem",    // 14px
-      "base": "1rem",     // 16px
-      "lg": "1.125rem",   // 18px
-      "xl": "1.25rem",    // 20px
-      "2xl": "1.5rem",    // 24px
-      "3xl": "1.875rem",  // 30px
-      "4xl": "2.25rem",   // 36px
-      "5xl": "2.625rem",  // 42px
-      "6xl": "4rem",      // 64px
+      xs: ".75rem", // 12px
+      sm: ".875rem", // 14px
+      base: "1rem", // 16px
+      lg: "1.125rem", // 18px
+      xl: "1.25rem", // 20px
+      "2xl": "1.5rem", // 24px
+      "3xl": "1.875rem", // 30px
+      "4xl": "2.25rem", // 36px
+      "5xl": "2.625rem", // 42px
+      "6xl": "4rem", // 64px
     },
     fontWeight: {
-      "hairline": 100,
-      "thin": 200,
-      "light": 300,
-      "normal": 400,
-      "medium": 500,
-      "semibold": 600,
-      "bold": 700,
-      "extrabold": 800,
-      "black": 900,
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
     },
     lineHeight: {
-      "none": 1,
-      "tight": 1.25,
-      "normal": 1.5,
-      "loose": 2,
+      none: 1,
+      tight: 1.25,
+      normal: 1.5,
+      loose: 2,
     },
     letterSpacing: {
-      "tight": "-0.05em",
-      "normal": "0",
-      "wide": "0.05em",
+      tight: "-0.05em",
+      normal: "0",
+      wide: "0.05em",
     },
-    textColor: theme => theme('colors'),
-    backgroundColor: theme => theme('colors'),
+    textColor: theme => theme("colors"),
+    backgroundColor: theme => theme("colors"),
     backgroundSize: {
-      "auto": "auto",
-      "cover": "cover",
-      "contain": "contain",
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
     },
     borderWidth: {
       default: "1px",
@@ -128,19 +97,19 @@ module.exports = {
       "8": "8px",
     },
     borderColor: theme => ({
-      default: theme('colors.cararra'),
-      ...theme('colors'),
+      default: theme("colors.cararra"),
+      ...theme("colors"),
     }),
     borderRadius: {
-      "none": "0",
-      "sm": ".125rem",
+      none: "0",
+      sm: ".125rem",
       default: ".25rem",
-      "lg": ".5rem",
-      "full": "9999px",
+      lg: ".5rem",
+      full: "9999px",
     },
     width: {
-      "auto": "auto",
-      "px": "1px",
+      auto: "auto",
+      px: "1px",
       "1": "0.25rem",
       "2": "0.5rem",
       "3": "0.75rem",
@@ -166,13 +135,13 @@ module.exports = {
       "4/5": "80%",
       "1/6": "16.66667%",
       "5/6": "83.33333%",
-      "full": "100%",
-      "screen": "100vw"
+      full: "100%",
+      screen: "100vw",
     },
     height: {
-      "auto": "auto",
+      auto: "auto",
       "0": 0,
-      "px": "1px",
+      px: "1px",
       "1": "0.25rem",
       "2": "0.5rem",
       "3": "0.75rem",
@@ -188,37 +157,37 @@ module.exports = {
       "40": "10rem",
       "48": "12rem",
       "64": "16rem",
-      "half": "50%",
-      "full": "100%",
-      "screen": "100vh"
+      half: "50%",
+      full: "100%",
+      screen: "100vh",
     },
     minWidth: {
       "0": "0",
-      "full": "100%",
+      full: "100%",
     },
     minHeight: {
       "0": "0",
-      "full": "100%",
-      "screen": "100vh"
+      full: "100%",
+      screen: "100vh",
     },
     maxWidth: {
-      "xs": "20rem",
-      "sm": "30rem",
-      "md": "40rem",
-      "lg": "50rem",
-      "xl": "60rem",
+      xs: "20rem",
+      sm: "30rem",
+      md: "40rem",
+      lg: "50rem",
+      xl: "60rem",
       "2xl": "70rem",
       "3xl": "80rem",
       "4xl": "90rem",
       "5xl": "100rem",
-      "full": "100%",
+      full: "100%",
     },
     maxHeight: {
-      "full": "100%",
-      "screen": "100vh",
+      full: "100%",
+      screen: "100vh",
     },
     padding: {
-      "px": "1px",
+      px: "1px",
       "0": "0",
       "1": "0.25rem",
       "2": "0.5rem",
@@ -237,8 +206,8 @@ module.exports = {
       "52": "13rem",
     },
     margin: {
-      "auto": "auto",
-      "px": "1px",
+      auto: "auto",
+      px: "1px",
       "0": "0",
       "1": "0.25rem",
       "2": "0.5rem",
@@ -270,14 +239,14 @@ module.exports = {
     },
     boxShadow: {
       default: "0 1px 2px 0 rgba(0,0,0,0.325)",
-      "md": "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)",
-      "lg": "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)",
-      "inner": "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
-      "outline": "0 0 0 3px rgba(52,144,220,0.5)",
-      "none": "none",
+      md: "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)",
+      lg: "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)",
+      inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
+      outline: "0 0 0 3px rgba(52,144,220,0.5)",
+      none: "none",
     },
     zIndex: {
-      "auto": "auto",
+      auto: "auto",
       "neg-10": -10,
       "0": 0,
       "10": 10,
@@ -286,7 +255,7 @@ module.exports = {
       "40": 40,
       "50": 50,
       "60": 60,
-      "70": 70
+      "70": 70,
     },
     opacity: {
       "0": "0",
@@ -299,14 +268,14 @@ module.exports = {
       "100": "1",
     },
     fill: {
-      "current": "currentColor",
+      current: "currentColor",
     },
     stroke: {
-      "current": "currentColor",
+      current: "currentColor",
     },
     container: {
-      center: true
-    }
+      center: true,
+    },
   },
   variants: {
     appearance: ["responsive"],
@@ -373,14 +342,14 @@ module.exports = {
     zIndex: ["responsive"],
   },
   plugins: [
-    require('tailwindcss-aspect-ratio')({
-      variants: ['responsive'],
+    require("tailwindcss-aspect-ratio")({
+      variants: ["responsive"],
       ratios: {
-        'square': [1, 1],
-        '16/9': [16, 9],
-        '4/3': [4, 3],
-        '21/9': [21, 9],
-      }
-    })
+        square: [1, 1],
+        "16/9": [16, 9],
+        "4/3": [4, 3],
+        "21/9": [21, 9],
+      },
+    }),
   ],
-}
+};
